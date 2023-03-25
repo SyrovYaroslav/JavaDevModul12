f2a8b0a992236618ebe03e638f4419e7a569a434
Yaroslav <y.s.syrovatka@student.khai.edu>
2023-03-25 16:58:34 +0200

    crate NoteService v1

---
+++ src/main/java/com/springcore/JavaDevModul12/JavaDevModul12Application.java
@@ -0,0 +1,13 @@
+package com.springcore.JavaDevModul12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevModul12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevModul12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcore/JavaDevModul12/JavaDevModul12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcore.JavaDevModul12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevModul12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
